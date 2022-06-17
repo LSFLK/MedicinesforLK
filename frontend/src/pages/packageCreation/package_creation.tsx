96f4c95bcb3d5f4bcf66398536b2ae1102c361b2
Nipuna Gunathilake <nipuna777@gmail.com>
2022-06-17 11:23:52 +0800

    add prettier and format existing files

--- frontend/src/pages/packageCreation/package_creation.tsx
+++ frontend/src/pages/packageCreation/package_creation.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { PageSelection } from "../../types/pages";
 import { Page } from "../layout/page";
 
-interface PackageCreationPageProps {
-
-}
+interface PackageCreationPageProps {}
 
 export function PackageCreation(params: PackageCreationPageProps) {
-    return (
-        <Page selection={PageSelection.PACKAGE_CREATION}>
-            <div>Package Creation</div>
-        </Page>
-    );
+  return (
+    <Page selection={PageSelection.PACKAGE_CREATION}>
+      <div>Package Creation</div>
+    </Page>
+  );
 }
