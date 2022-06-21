94a4c19e4f41450d84aac9287384ee437f87279a
YaiyaSam <kalsha.sam26@gmail.com>
2022-06-21 13:34:40 +0530

    Implement the navbar component

--- donor-portal/src/components/navbar/index.tsx
+++ donor-portal/src/components/navbar/index.tsx
@@ -3,41 +3,18 @@ import "./styles.css";
 
 export function NavBar() {
   return (
-    <nav className="navbar navbar--fixed-top">
-      <div className="navbar__inner">
-        <div className="navbar__items">
-          <button
-            aria-label="Navigation bar toggle"
-            className="navbar__toggle clean-btn"
-            type="button"
-            tabIndex={0}
-          >
-            <svg width="30" height="30" viewBox="0 0 30 30" aria-hidden="true">
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                strokeWidth="2"
-                d="M4 7h22M4 15h22M4 23h22"
-              ></path>
-            </svg>
-          </button>
-          <a className="navbar__brand" href="/">
-            <div className="navbar__logo">
-              <img
-                src="/assets/images/elixirLogo4.png"
-                alt="Logo of a red cross"
-                className="themedImage--light"
-              />
-            </div>
-            <b className="navbar__title">Elixir</b>
-          </a>
-        </div>
-        <div className="navbar__items navbar__items--right">
-          <Link to="/about-us" className="navbar__item navbar__link" >About</Link>
-          <Link to="/login" className="navbar__item navbar__link" >Login</Link>
-        </div>
-      </div>
+    <nav className="navbar">
+     <div className="logo"></div>
+     <div className="nav-links">
+       <Link to="/about-us" className="nav-link"> About </Link><span>|</span>
+       <Link to="/donors" className="nav-link"> Donors </Link><span>|</span>
+       <Link to="" className="nav-link"> Request Medicine </Link><span>|</span>
+       <Link to="" className="nav-link"> Medical Suppliers </Link><span>|</span>
+       <Link to="" className="nav-link"> Newsroom </Link>
+       <Link to="/login" className="login-btn"> Login </Link>
+
+
+     </div>
     </nav>
   );
 }
