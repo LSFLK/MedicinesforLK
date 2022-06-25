3d319a346d2233890e13561ac4b6f40bd15f1839
Jayasanka <jayasanka.sack@gmail.com>
2022-06-25 17:14:59 +0530

    Implement the pledge status page

--- admin-portal/src/App.tsx
+++ admin-portal/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { NavBar } from "./components/navbar/navbar";
+import { NavBar } from "./components";
 import { Home } from "./pages/home/home";
 import { NeedsUpload } from "./pages/needsupload/needsupload";
 import "./App.css";
 import NewAidPackage from "./pages/aidPackage/newAidPackage/NewAidPackage";
 import {PackageDetails} from "./pages/packageDetails/packageDetails";
+import PledgeStatus from "./pages/pledgeStatus/pledgeStatus";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path="creation" element={<NewAidPackage />} />
             <Route path="needsupload" element={<NeedsUpload />} />
             <Route path="packages/:packageId" element={<PackageDetails />} />
+            <Route path="packages/:packageId/pledge-status" element={<PledgeStatus />} />
           </Routes>
         </BrowserRouter>
       </div>
