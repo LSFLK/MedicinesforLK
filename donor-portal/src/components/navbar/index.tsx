2653339014f862f21ae295c5667104603c38b809
YaiyaSam <kalsha.sam26@gmail.com>
2022-07-05 11:32:19 +0530

    code formatting

--- donor-portal/src/components/navbar/index.tsx
+++ donor-portal/src/components/navbar/index.tsx
@@ -4,15 +4,37 @@ import "./styles.css";
 export function NavBar() {
   return (
     <nav className="navbar">
-     <div className="logo"></div>
-     <div className="nav-links">
-       <Link to="/about-us" className="nav-link"> About </Link><span>|</span>
-       <Link to="/donors" className="nav-link"> Donors </Link><span>|</span>
-       <Link to="" className="nav-link"> Request Medicine </Link><span>|</span>
-       <Link to="/suppliers" className="nav-link"> Medical Suppliers </Link><span>|</span>
-       <Link to="" className="nav-link"> Newsroom </Link>
-       <Link to="/login" className="login-btn"> Login </Link>
-     </div>
+      <div className="logo"></div>
+      <div className="nav-links">
+        <Link to="/about-us" className="nav-link">
+          {" "}
+          About{" "}
+        </Link>
+        <span>|</span>
+        <Link to="/donors" className="nav-link">
+          {" "}
+          Donors{" "}
+        </Link>
+        <span>|</span>
+        <Link to="" className="nav-link">
+          {" "}
+          Request Medicine{" "}
+        </Link>
+        <span>|</span>
+        <Link to="/suppliers" className="nav-link">
+          {" "}
+          Medical Suppliers{" "}
+        </Link>
+        <span>|</span>
+        <Link to="" className="nav-link">
+          {" "}
+          Newsroom{" "}
+        </Link>
+        <Link to="/login" className="login-btn">
+          {" "}
+          Login{" "}
+        </Link>
+      </div>
     </nav>
   );
 }
