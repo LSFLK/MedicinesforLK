fbfb91c312ec14340bdd42fbbcf7dc5bca774f7a
Gravewalker666 <heshanandrews99@gmail.com>
2022-07-07 21:48:30 +0530

    Prettierfy the code

--- admin-portal/src/pages/home/home.tsx
+++ admin-portal/src/pages/home/home.tsx
@@ -1,14 +1,16 @@
-import React, {useEffect, useRef, useState} from "react";
-import {PageSelection} from "../../types/pages";
-import {Page} from "../../layout/page";
+import React, { useEffect, useRef, useState } from "react";
+import { PageSelection } from "../../types/pages";
+import { Page } from "../../layout/page";
 import "./home.css";
-import {AidPackageService} from "../../apis/services/AidPackageService";
-import {AidPackage} from "../../types/AidPackage";
-import {useNavigate} from "react-router-dom";
+import { AidPackageService } from "../../apis/services/AidPackageService";
+import { AidPackage } from "../../types/AidPackage";
+import { useNavigate } from "react-router-dom";
 
 export function Home() {
   const aidPackages = useRef<AidPackage[]>([]);
-  const [filteredAidPackages, setFilteredAidPackages] = useState<AidPackage[]>([]);
+  const [filteredAidPackages, setFilteredAidPackages] = useState<AidPackage[]>(
+    []
+  );
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -24,17 +26,20 @@ export function Home() {
   }, []);
 
   function handlePledgesButton(packageId: number) {
-    navigate(`/packages/${packageId}`)
+    navigate(`/packages/${packageId}`);
   }
 
   function handleDetailsButton(packageId: number) {
-    navigate(`/packages/${packageId}/pledge-status`)
+    navigate(`/packages/${packageId}/pledge-status`);
   }
 
   function handleSearch(keyword: string) {
-    keyword = keyword.trim().toLowerCase()
+    keyword = keyword.trim().toLowerCase();
     const newlyFilteredPackages = aidPackages.current.filter((aidPackage) => {
-      return aidPackage.name.toLowerCase().includes(keyword) || aidPackage.status.toLowerCase().includes(keyword);
+      return (
+        aidPackage.name.toLowerCase().includes(keyword) ||
+        aidPackage.status.toLowerCase().includes(keyword)
+      );
     });
     setFilteredAidPackages(newlyFilteredPackages);
   }
@@ -49,9 +54,9 @@ export function Home() {
           <div className="searchContainer">
             <img src="/assets/svg/search_icon.svg" />
             <input
-                placeholder="Search"
-                className="textField"
-                onChange={(event) => handleSearch(event.target.value)}
+              placeholder="Search"
+              className="textField"
+              onChange={(event) => handleSearch(event.target.value)}
             />
           </div>
         </div>
@@ -68,35 +73,40 @@ export function Home() {
             </thead>
             <tbody>
               {filteredAidPackages.length == 0 && (
-                  <tr>
-                    <td colSpan={5}>No data found</td>
-                  </tr>
+                <tr>
+                  <td colSpan={5}>No data found</td>
+                </tr>
               )}
               {filteredAidPackages.map((aidPackage) => {
                 return (
-                    <tr key={aidPackage.packageID}>
-                      <td>{aidPackage.name}</td>
-                      <td>{aidPackage.status}</td>
-                      <td>
-                        {(
-                            (aidPackage.receivedAmount / aidPackage.goalAmount || 0) * 100
-                        ).toFixed(0)}
-                        %
-                      </td>
-                      <td>--</td>
-                      <td>
-                        <button
-                            onClick={() => handleDetailsButton(aidPackage.packageID)}
-                        >
-                          Details
-                        </button>
-                        <button
-                            onClick={() => handlePledgesButton(aidPackage.packageID)}
-                        >
-                          Pledges
-                        </button>
-                      </td>
-                    </tr>
+                  <tr key={aidPackage.packageID}>
+                    <td>{aidPackage.name}</td>
+                    <td>{aidPackage.status}</td>
+                    <td>
+                      {(
+                        (aidPackage.receivedAmount / aidPackage.goalAmount ||
+                          0) * 100
+                      ).toFixed(0)}
+                      %
+                    </td>
+                    <td>--</td>
+                    <td>
+                      <button
+                        onClick={() =>
+                          handleDetailsButton(aidPackage.packageID)
+                        }
+                      >
+                        Details
+                      </button>
+                      <button
+                        onClick={() =>
+                          handlePledgesButton(aidPackage.packageID)
+                        }
+                      >
+                        Pledges
+                      </button>
+                    </td>
+                  </tr>
                 );
               })}
             </tbody>
