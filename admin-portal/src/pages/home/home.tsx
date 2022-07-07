adc1af723c9551442f1e9007b998aa4725e6c36f
Gravewalker666 <heshanandrews99@gmail.com>
2022-07-07 21:45:24 +0530

    Implement aid package search on admin portal home

--- admin-portal/src/pages/home/home.tsx
+++ admin-portal/src/pages/home/home.tsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { PageSelection } from "../../types/pages";
-import { Page } from "../../layout/page";
+import React, {useEffect, useRef, useState} from "react";
+import {PageSelection} from "../../types/pages";
+import {Page} from "../../layout/page";
 import "./home.css";
-import { AidPackageService } from "../../apis/services/AidPackageService";
+import {AidPackageService} from "../../apis/services/AidPackageService";
 import {AidPackage} from "../../types/AidPackage";
 import {useNavigate} from "react-router-dom";
 
 export function Home() {
-  const [aidPackages, setAidPackages] = useState<AidPackage[]>([]);
+  const aidPackages = useRef<AidPackage[]>([]);
+  const [filteredAidPackages, setFilteredAidPackages] = useState<AidPackage[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await AidPackageService.getAidPackages();
       if (response.data) {
-        setAidPackages(response.data);
+        aidPackages.current = response.data;
+        setFilteredAidPackages(response.data);
       }
     };
 
@@ -29,6 +31,14 @@ export function Home() {
     navigate(`/packages/${packageId}/pledge-status`)
   }
 
+  function handleSearch(keyword: string) {
+    keyword = keyword.trim().toLowerCase()
+    const newlyFilteredPackages = aidPackages.current.filter((aidPackage) => {
+      return aidPackage.name.toLowerCase().includes(keyword) || aidPackage.status.toLowerCase().includes(keyword);
+    });
+    setFilteredAidPackages(newlyFilteredPackages);
+  }
+
   return (
     <Page selection={PageSelection.HOME}>
       <div className="pageContent">
@@ -38,48 +48,58 @@ export function Home() {
         <div className="packageTableSearch">
           <div className="searchContainer">
             <img src="/assets/svg/search_icon.svg" />
-            <input placeholder="Search" className="textField" />
+            <input
+                placeholder="Search"
+                className="textField"
+                onChange={(event) => handleSearch(event.target.value)}
+            />
           </div>
         </div>
         <div className="packageTableContainer">
           <table>
-            <tr>
-              <th>Aid Package</th>
-              <th>Status</th>
-              <th>Pledges</th>
-              <th>Supplier</th>
-              <th></th>
-            </tr>
-            {aidPackages.length == 0 && (
-                <div>No data found</div>
-            )}
-            {aidPackages.map((aidPackage) => {
-              return (
+            <thead>
+              <tr>
+                <th>Aid Package</th>
+                <th>Status</th>
+                <th>Pledges</th>
+                <th>Supplier</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredAidPackages.length == 0 && (
                   <tr>
-                    <td>{aidPackage.name}</td>
-                    <td>{aidPackage.status}</td>
-                    <td>
-                      {(
-                          (aidPackage.receivedAmount / aidPackage.goalAmount || 0) * 100
-                      ).toFixed(0)}
-                      %
-                    </td>
-                    <td>--</td>
-                    <td>
-                      <button
-                          onClick={() => handleDetailsButton(aidPackage.packageID)}
-                      >
-                        Details
-                      </button>
-                      <button
-                          onClick={() => handlePledgesButton(aidPackage.packageID)}
-                      >
-                        Pledges
-                      </button>
-                    </td>
+                    <td colSpan={5}>No data found</td>
                   </tr>
-              );
-            })}
+              )}
+              {filteredAidPackages.map((aidPackage) => {
+                return (
+                    <tr key={aidPackage.packageID}>
+                      <td>{aidPackage.name}</td>
+                      <td>{aidPackage.status}</td>
+                      <td>
+                        {(
+                            (aidPackage.receivedAmount / aidPackage.goalAmount || 0) * 100
+                        ).toFixed(0)}
+                        %
+                      </td>
+                      <td>--</td>
+                      <td>
+                        <button
+                            onClick={() => handleDetailsButton(aidPackage.packageID)}
+                        >
+                          Details
+                        </button>
+                        <button
+                            onClick={() => handlePledgesButton(aidPackage.packageID)}
+                        >
+                          Pledges
+                        </button>
+                      </td>
+                    </tr>
+                );
+              })}
+            </tbody>
           </table>
         </div>
       </div>
