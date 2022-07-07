8df13cf62a864299feae060e115dd3de7f2a08bf
Heshan Andrews <45477334+Gravewalker666@users.noreply.github.com>
2022-07-07 22:14:12 +0530

    Implement aid package search on admin portal home (#62)
    * Change the admin homepage structure to render packages in place
    
    * Implement aid package search on admin portal home
    
    * Prettierfy the code

--- admin-portal/src/pages/home/tableRow/emptyRow.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactElement, useEffect, useState } from "react";
-
-interface EmptyRowProps {}
-
-export function EmptyRow(props: EmptyRowProps) {
-  return <tr>No Data Found</tr>;
-}
