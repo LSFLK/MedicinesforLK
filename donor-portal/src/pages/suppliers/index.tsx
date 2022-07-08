2f562d8badfd1a700b125138eaa58abda9463a4e
Randila Premarathne <randilamenukapremarathne@gmail.com>
2022-07-08 13:59:19 +0530

    Reformat the donor portal source

--- donor-portal/src/pages/suppliers/index.tsx
+++ donor-portal/src/pages/suppliers/index.tsx
@@ -2,7 +2,7 @@ import { FullBleedContainer } from "../layout/full-bleed-container";
 import { HeaderImage } from "../layout/header-image";
 import { Page } from "../layout/page";
 import { InfoBoxContainer } from "../layout/info-box-container";
-import { InfoBox } from "../layout/info-box" 
+import { InfoBox } from "../layout/info-box";
 import "./styles.css";
 
 export function Suppliers() {
@@ -13,35 +13,32 @@ export function Suppliers() {
       </HeaderImage>
       <div className="text-section">
         <p>
-        We are continuously growing our network of hospitals and pharmaceutical 
-        suppliers. Please contact us at <a href="mailto: inquiries@redcross.lk">inquiries@redcross.lk</a>  for details on 
-        how to participate.
+          We are continuously growing our network of hospitals and
+          pharmaceutical suppliers. Please contact us at{" "}
+          <a href="mailto: inquiries@redcross.lk">inquiries@redcross.lk</a> for
+          details on how to participate.
         </p>
       </div>
       <InfoBoxContainer>
-            <InfoBox 
-                  iconUrl="/assets/images/doctor.jpg"
-                  text="ELIXIR provides an easy to use application to manage all your urgent supply needs. Contact us for details on how 
+        <InfoBox
+          iconUrl="/assets/images/doctor.jpg"
+          text="ELIXIR provides an easy to use application to manage all your urgent supply needs. Contact us for details on how 
                         to create an account"
-                  heading="Hospitals/Doctors"
-                  link="#"
-            ></InfoBox>
-            <InfoBox 
-                  iconUrl="/assets/images/pharmacy.jpg"
-                  text="ELIXIR would like to partner with you to source the needs of our hospitals and doctors. Contact us to onboard your 
+          heading="Hospitals/Doctors"
+          link="#"
+        ></InfoBox>
+        <InfoBox
+          iconUrl="/assets/images/pharmacy.jpg"
+          text="ELIXIR would like to partner with you to source the needs of our hospitals and doctors. Contact us to onboard your 
                         inventory."
-                  heading="Pharmaceutical Suppliers"
-                  link="#"
-            ></InfoBox>
-            <p className="inquiry">For all inquiries please emails us at  <a href="mailto: inquiries@redcross.lk">inquires@redcross.lk</a></p>
-
-            
-
+          heading="Pharmaceutical Suppliers"
+          link="#"
+        ></InfoBox>
+        <p className="inquiry">
+          For all inquiries please emails us at{" "}
+          <a href="mailto: inquiries@redcross.lk">inquires@redcross.lk</a>
+        </p>
       </InfoBoxContainer>
-      
-    
     </Page>
   );
 }
-
-
