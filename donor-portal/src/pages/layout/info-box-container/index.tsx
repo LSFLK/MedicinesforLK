b544d2f943f0d115cfe609c9b203d52bb0ed2f08
RandilaP <106733522+RandilaP@users.noreply.github.com>
2022-07-08 14:04:42 +0530

    Reformat the donor portal source (#68)

--- donor-portal/src/pages/layout/info-box-container/index.tsx
+++ donor-portal/src/pages/layout/info-box-container/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./styles.css";
 
-export function InfoBoxContainer({children, style = {}, className}: {children?: React.ReactNode, style?: React.CSSProperties, className?: string}) {
+export function InfoBoxContainer({
+  children,
+  style = {},
+  className,
+}: {
+  children?: React.ReactNode;
+  style?: React.CSSProperties;
+  className?: string;
+}) {
   return (
-    <div className={`info-box-container ${className}`} style={style}>{children}</div>
+    <div className={`info-box-container ${className}`} style={style}>
+      {children}
+    </div>
   );
 }
