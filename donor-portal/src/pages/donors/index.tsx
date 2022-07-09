e4fc7c09fca401083b8f5f23e903bc8b3baa8c68
RandilaP <106733522+RandilaP@users.noreply.github.com>
2022-07-09 13:11:16 +0530

    Change email address on donors (#98)

--- donor-portal/src/pages/donors/index.tsx
+++ donor-portal/src/pages/donors/index.tsx
@@ -47,7 +47,7 @@ export function Donors() {
         ></InfoBox>
         <p className="inquiry">
           For all inquiries please emails us at{" "}
-          <a href="mailto: inquires@redcross.lk">inquires@redcross.lk</a>
+          <a href="mailto: inquiries@redcross.lk">inquiries@redcross.lk</a>
         </p>
       </InfoBoxContainer>
     </Page>
