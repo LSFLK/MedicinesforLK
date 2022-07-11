f328d00555f483ffa5b10d35c9be0b3beb3892ee
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2022-07-11 17:46:24 +0530

    Display missing details on create aid package view (#115)

--- admin-portal/src/types/Quotation.ts
+++ admin-portal/src/types/Quotation.ts
@@ -6,8 +6,16 @@ export interface Quotation {
   itemID: number;
   brandName: string;
   availableQuantity: number;
-  period: number;
-  expiryDate: number;
+  expiryDate: {
+    year: number;
+    month: number;
+    day: number;
+  };
+  period: {
+    year: number;
+    month: number;
+    day: number;
+  };
   regulatoryInfo: string;
   unitPrice: number;
   supplier: Supplier;
