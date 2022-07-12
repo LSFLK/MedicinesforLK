68668d7d8b0e6f6ae1013192f358a39da9f866e4
Jayasanka <jayasanka.sack@gmail.com>
2022-07-12 17:57:06 +0530

    Load aid packages from the API

---
+++ donor-portal/src/types/DonarAidPackagePledge.ts
@@ -0,0 +1,6 @@
+export interface DonorAidPackagePledge {
+  id: number;
+  name: string;
+  amount: number;
+  status: string;
+}
---
+++ donor-portal/src/types/DonorAidPackageOrderItem.ts
@@ -0,0 +1,13 @@
+import { Quotation } from "./Quotation";
+
+export interface PartialAidPackageItem {}
+
+export interface AidPackageItem {
+  packageItemID: number;
+  packageID: number;
+  quotationID: number;
+  quantity: number;
+  totalAmount: number;
+  needID: number;
+  quotation: Quotation;
+}
