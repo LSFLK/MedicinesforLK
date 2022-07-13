219c786a9c647ae2de83cbe057e1af3ed0a1a85b
Gravewalker666 <heshanandrews99@gmail.com>
2022-07-13 12:07:25 +0530

    Add pledge and update pledge functions to donor portal

--- donor-portal/src/apis/services/AidPackageService.ts
+++ donor-portal/src/apis/services/AidPackageService.ts
@@ -1,6 +1,7 @@
 import { adminHttp, donorHttp } from "../httpCommon";
 import { AidPackage } from "../../types/AidPackage";
 import { AidPackageUpdateComment } from "../../types/AidPackageUpdateComment";
+import {Pledge} from "../../types/Pledge";
 
 export class AidPackageService {
   static getAidPackages() {
@@ -19,5 +20,23 @@ export class AidPackageService {
     return adminHttp.get<AidPackageUpdateComment[]>(
       `aidpackages/${packageID}/updatecomments`
     );
+  }
+
+  static getDonorPledgesByAidPackage(donorId: string, packageId: string) {
+    return donorHttp.get<Pledge[]>(`${donorId}/aidpackages/${packageId}/pledges`);
+  }
+
+  static postPledge(donorId: string, packageId: string, pledge: Pledge) {
+    return donorHttp.post(
+      `${donorId}/aidpackages/${packageId}/pledge`,
+      pledge
+    );
+  }
+
+  static updatePledge(donorId: string, packageId: string, pledgeID: number, updatedPledge: Pledge) {
+    return donorHttp.put(
+      `${donorId}/aidpackages/${packageId}/pledges/${pledgeID}`,
+      updatedPledge
+    )
   }
 }
--- donor-portal/src/pages/package/index.tsx
+++ donor-portal/src/pages/package/index.tsx
@@ -1,46 +1,99 @@
 /**
  * main page for displaying aid package details
  */
-import { useEffect, useState } from "react";
-import { useParams, Redirect } from "react-router-dom";
-import { SimpleProgressBar } from "../../components/progress-bar";
-import { Page } from "../layout/page";
+import React, {FormEvent, useContext, useEffect, useState} from "react";
+import {useHistory, useParams} from "react-router-dom";
+import {SimpleProgressBar} from "../../components/progress-bar";
+import {Page} from "../layout/page";
 import "./styles.css";
-import { AidPackageService } from "../../apis/services/AidPackageService";
-import { AidPackage } from "../../types/AidPackage";
+import {AidPackageService} from "../../apis/services/AidPackageService";
+import {AidPackage} from "../../types/AidPackage";
 import OrderItemsTable from "./components/orderItemsTable/orderItemsTable";
 import PackageStatus from "./components/packageStatus";
 import UpdateComments from "./components/updateComments";
-import { AidPackageUpdateComment } from "../../types/AidPackageUpdateComment";
+import {AidPackageUpdateComment} from "../../types/AidPackageUpdateComment";
+import UserContext from "../../userContext";
+import {Pledge} from "../../types/Pledge";
 
 export function AidPackageDetailsPage() {
-  const { id: packageId } = useParams<{ id: string }>();
+  const {id: packageId} = useParams<{ id: string }>();
+  const userId = useContext(UserContext);
   const [aidPackage, setAidPackage] = useState<AidPackage>();
-  const [updateComments, setUpdateComments] = useState<
-    AidPackageUpdateComment[]
-  >([]);
+  const [pledge, setPledge] = useState<Pledge | null>(null);
+  const [updateComments, setUpdateComments] = useState<AidPackageUpdateComment[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const history = useHistory();
+  const navigate = (path: string) => {
+    history.push(path);
+  };
 
   useEffect(() => {
     fetchAidPackage();
     fetchUpdateComments();
   }, []);
 
+  useEffect(() => {
+    if (userId != null) fetchPledge(userId, packageId);
+  }, [userId])
+
   const fetchAidPackage = async () => {
     setIsLoading(true);
-    const { data } = await AidPackageService.getAidPackage(packageId);
+    const {data} = await AidPackageService.getAidPackage(packageId);
     setAidPackage(data);
     setIsLoading(false);
   };
 
   const fetchUpdateComments = async () => {
-    const { data } = await AidPackageService.getUpdateComments(packageId);
+    const {data} = await AidPackageService.getUpdateComments(packageId);
     setUpdateComments(data);
   };
 
+  const fetchPledge = async (donorId: string, packageId: string) => {
+    const { data } = await AidPackageService.getDonorPledgesByAidPackage(donorId, packageId);
+    if (data.length != 0) setPledge(data[0]);
+  }
+
   const handleDonateClick = () => {
-    //TODO: Implement the expected behaviour
+    navigate('/donate-now');
   };
+
+  const handlePledgeSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
+    const amount = form.get("amount")?.toString();
+    if (amount == undefined || userId == null || amount === "") return;
+    if (!window.confirm(`Please confirm your pledge of ${amount}`)) return;
+    const newPledge: Pledge = {
+      pledgeID: 0,
+      donorID: parseInt(userId),
+      packageID: parseInt(packageId),
+      amount: parseInt(amount),
+      status: Pledge.Status.Pledged,
+      donor: null
+    };
+    await AidPackageService.postPledge(userId, packageId, newPledge);
+    fetchPledge(userId, packageId);
+    fetchAidPackage();
+  };
+
+  const handlePledgeUpdate = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
+    const amount = form.get("amount")?.toString();
+    if (amount == undefined || userId == null || pledge == null) return;
+    if (!window.confirm(`Please confirm your pledge of ${amount}`)) return;
+    const updatedPledge: Pledge = {
+      pledgeID: pledge.pledgeID,
+      donorID: parseInt(userId),
+      packageID: parseInt(packageId),
+      amount: parseInt(amount),
+      status: pledge.status,
+      donor: null
+    };
+    await AidPackageService.updatePledge(userId, packageId, pledge.pledgeID, updatedPledge);
+    fetchPledge(userId, packageId);
+    fetchAidPackage();
+  }
 
   return (
     <>
@@ -78,16 +131,46 @@ export function AidPackageDetailsPage() {
                 max={aidPackage.goalAmount as number}
                 className="aid-pacakge-progress-bar"
               />
-              <button
-                className="btn aid-package-donate-btn"
-                onClick={handleDonateClick}
-              >
-                Donate
-              </button>
+              { userId == null && (
+                <button
+                  className="btn aid-package-donate-btn"
+                  onClick={handleDonateClick}
+                >
+                  Donate
+                </button>
+              )}
             </div>
-            <OrderItemsTable items={aidPackage.aidPackageItems} />
-            <PackageStatus currentStatus={aidPackage.status} />
-            <UpdateComments comments={updateComments} />
+            <div>
+              {userId != null &&
+                (
+                  <>
+                    {pledge != null ? (
+                      <>
+                        <p>You've pledged {pledge.amount} on this package</p>
+                        {aidPackage.status == AidPackage.Status.Published && (
+                            <form onSubmit={handlePledgeUpdate}>
+                              <input type="number" name="amount" defaultValue={pledge.amount}/>
+                              <button>Update pledge</button>
+                            </form>
+                        )}
+                      </>
+                    ) : (
+                      <>
+                        {aidPackage.status == AidPackage.Status.Published && (
+                          <form onSubmit={handlePledgeSubmit}>
+                            <input type="number" name="amount" placeholder="1000"/>
+                            <button>Pledge</button>
+                          </form>
+                        )}
+                      </>
+                    )}
+                  </>
+                )
+              }
+            </div>
+            <OrderItemsTable items={aidPackage.aidPackageItems}/>
+            <PackageStatus currentStatus={aidPackage.status}/>
+            <UpdateComments comments={updateComments}/>
           </div>
         </Page>
       )}
