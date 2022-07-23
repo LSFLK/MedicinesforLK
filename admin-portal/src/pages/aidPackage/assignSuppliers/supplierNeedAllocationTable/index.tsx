39b76dcb38e63125199ff8473eb0c6a86381548d
Anjula Shanaka <anjulashanaka@gmail.com>
2022-07-23 18:32:48 +0530

    Update the date format in create aid package (#242)

--- admin-portal/src/pages/aidPackage/assignSuppliers/supplierNeedAllocationTable/index.tsx
+++ admin-portal/src/pages/aidPackage/assignSuppliers/supplierNeedAllocationTable/index.tsx
@@ -1,5 +1,6 @@
 import { getDraftAidPackageKey } from "helpers/aidPackageHelper";
 import { formatMoney } from "helpers/formatter";
+import moment from "moment";
 import { DraftAidPackages, NeedAssignment } from "pages/aidPackage/aidPackage";
 import { useMemo, useState, useEffect } from "react";
 import { useTable, CellValue } from "react-table";
@@ -28,8 +29,12 @@ export function SupplierNeedAllocationTable({
         return {
           supplier: quote.supplier.name,
           brandName: quote.brandName,
-          expiryDate: `${quote.expiryDate.day}/${quote.expiryDate.month}/${quote.expiryDate.year}`,
-          period: `${quote.period.day}/${quote.period.month}/${quote.period.year}`,
+          expiryDate: moment(
+            `${quote.expiryDate.day}/${quote.expiryDate.month}/${quote.expiryDate.year}`
+          ).format("MM/DD/YYYY"),
+          period: moment(
+            `${quote.period.day}/${quote.period.month}/${quote.period.year}`
+          ).format("MM/DD/YYYY"),
           quantity,
           max: Math.min(requiredQuantity, quote.availableQuantity),
           supplierID: quote.supplierID,
@@ -52,11 +57,21 @@ export function SupplierNeedAllocationTable({
         accessor: "brandName",
       },
       {
-        Header: "Period",
+        Header: () => (
+          <>
+            <p>Period</p>
+            <small className="table-sub-header">DD/MM/YY</small>
+          </>
+        ),
         accessor: "period",
       },
       {
-        Header: "Expiry Date",
+        Header: () => (
+          <>
+            <p>Expiry Date</p>
+            <small className="table-sub-header">DD/MM/YY</small>
+          </>
+        ),
         accessor: "expiryDate",
       },
       {
