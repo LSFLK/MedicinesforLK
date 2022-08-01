3b55998538dfcbe597ed520973801686ec4c2feb
dinuka-rp <drpiyadigama@gmail.com>
2022-08-01 18:23:11 +0530

    remove unused variables - fix linting issue in build

--- admin-portal/src/pages/packageDetails/components/orderItemsTable/orderItemsTable.tsx
+++ admin-portal/src/pages/packageDetails/components/orderItemsTable/orderItemsTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { AidPackage } from "../../../../types/AidPackage";
 import { AidPackageItem } from "../../../../types/DonorAidPackageOrderItem";
 import "./orderItemsTable.css";
