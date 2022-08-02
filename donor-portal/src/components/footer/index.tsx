7bacf517d5278b3d77533edd54b1ec64f3082f4b
RandilaP <106733522+RandilaP@users.noreply.github.com>
2022-08-02 07:06:58 +0530

    Change footer design (#322)
    * Edit footer UI changes
    
    * Rearrange footer
    
    * Edit changes and classes

--- donor-portal/src/components/footer/index.tsx
+++ donor-portal/src/components/footer/index.tsx
@@ -6,29 +6,40 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-black">
-        <div className="footer-black-top">
-          <img
-            src="/assets/images/about-us/sl-red-cross-logo.jpg"
-            alt="SL Red Cross Logo"
-          />
+      <div className="footer-gray">
+        <div className="footer-gray-top">
           <div className="icons">
-            <p>FOLLOW</p>
-            <a href="https://www.facebook.com/pages/Sri-Lanka-Red-Cross-Society/133902706641597">
+            <a
+              href="https://www.facebook.com/pages/Sri-Lanka-Red-Cross-Society/133902706641597"
+              target="_blank"
+              rel="noreferrer"
+            >
               <FaFacebookF className="icon" />
             </a>
-            <a href="https://twitter.com/SLRedCross">
+            <a
+              href="https://twitter.com/SLRedCross"
+              target="_blank"
+              rel="noreferrer"
+            >
               <FaTwitter className="icon" />
             </a>
-            <a href="https://instagram.com/slredcross?igshid=YmMyMTA2M2Y=">
+            <a
+              href="https://instagram.com/slredcross?igshid=YmMyMTA2M2Y="
+              target="_blank"
+              rel="noreferrer"
+            >
               <FaInstagram className="icon" />
             </a>
-            <a href="https://www.youtube.com/user/srilankanredcross">
+            <a
+              href="https://www.youtube.com/user/srilankanredcross"
+              target="_blank"
+              rel="noreferrer"
+            >
               <FaYoutube className="icon" />
             </a>
           </div>
         </div>
-        <div className="footer-black-bottom">
+        <div className="footer-geay-middle">
           <Link to="/about-us" className="text-link">
             About
           </Link>
@@ -46,10 +57,14 @@ export default function Footer() {
           </Link>
         </div>
       </div>
-      <div className="footer-gray">
+      <div className="footer-gray-bottom">
         <p>
-          Copyright © <a href="www.elixir.redcross.lk">elixir.redcross.lk </a>|
-          All rights reserved
+          All content copyright © 2022 Sri Lanka Red Cross. All rights reserved.{" "}
+          |
+          <a href=" https://www.redcross.lk/who-we-are/our-legal-status-in-sri-lanka/">
+            {" "}
+            Our Legal Status in Sri Lanka
+          </a>
         </p>
       </div>
     </footer>
