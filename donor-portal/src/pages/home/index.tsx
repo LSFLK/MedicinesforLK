67a4b1015e1e3f602151bdb7ecdf44704d8a81f9
RandilaP <106733522+RandilaP@users.noreply.github.com>
2022-08-02 13:15:04 +0530

    Update the homepage (#328)
    * Edit homepage UI changes
    
    * Arrange logo positions
    
    * Link logos to sites
    
    * Adjust reviews
    
    * Link medical needs

--- donor-portal/src/pages/home/index.tsx
+++ donor-portal/src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import HeaderImage from "../layout/header-image";
 import Page from "../layout/page";
 import "./styles.css";
@@ -25,43 +26,111 @@ export default function Home() {
       <div className="home-description">
         <div className="image-container">
           <img
-            src="assets/images/donating.png"
+            src="assets/images/slredcross_logo.png"
             alt="donating medical supplies"
           />
         </div>
         <div className="home-description-text">
           <h3>An Acute Medical Emergency</h3>
           <p>
-            <strong>
-              Sri Lanka lacks a coordinated response to address the overall
-              medical needs of hospitals. Given the limited resources it is
-              essential to optimize the demand and distribution of supplies to
-              help avoid possible duplication, address gaps and avoid
-              detrimental outcomes to citizens most in need.
-            </strong>
+            Sri Lanka lacks a coordinated response to address the overall
+            medical needs of hospitals. Given the limited resources it is
+            essential to optimize the demand and distribution of supplies to
+            help avoid possible duplication, address gaps and avoid detrimental
+            outcomes to citizens most in need.
           </p>
           <h3>An Alternative Approach</h3>
           <p>
-            <strong>
-              ELIXIR is a platform designed to meet this need. It is operated by
-              the Sri Lanka Red Cross along with a network of volunteers and
-              professionals to help improve the efficacy of donations. In
-              partnership with the Sri Lanka Medical Supplies Division we have
-              compiled aid packages that aggregate needs across multiple
-              hospitals to help achieve bulk price discounts from suppliers. Our
-              volunteers help negotiate prices and coordinate invoicing and
-              shipping logistics. ELIXIR facilitates transparency and
-              accountability throughout the entire supply chain.
-            </strong>
+            Elixir is a platform designed to meet this need. It is operated by
+            the Sri Lanka Red Cross along with a network of volunteers and
+            professionals to help improve the efficacy of donations. In
+            partnership with the Sri Lanka Medical Supplies Division we have
+            compiled aid packages that aggregate needs across multiple hospitals
+            to help achieve bulk price discounts from suppliers. Our volunteers
+            help negotiate prices and coordinate invoicing and shipping
+            logistics. Elixir facilitates transparency and accountability
+            throughout the entire supply chain.
           </p>
           <h3>Please Donate</h3>
           <p>
-            <strong>
-              Together we can do more. We are accepting donations from both
-              organizations and individuals. Check out our aid packages below
-              and click on the Donate button to get started.
-            </strong>
+            Together we can do more. We are accepting donations from both
+            organizations and individuals. Check out{" "}
+            <Link to="/medical-needs">medical needs</Link> and click on the
+            Donate button to get started.
           </p>
+        </div>
+      </div>
+      <div className="current-donors">
+        <h2>Current Donors</h2>
+        <div className="logo-container">
+          <div className="donor-logo">
+            <a href="https://www.icrc.org/en" target="_blank" rel="noreferrer">
+              <img
+                src="assets/images/current-donors/icrc_logo.png"
+                alt="icrc-logo"
+              />
+            </a>
+          </div>
+          <div className="donor-logo">
+            <a href="https://www.ifrc.org/" target="_blank" rel="noreferrer">
+              <img
+                src="assets/images/current-donors/ifrc_logo.png"
+                alt="ifrc-logo"
+              />
+            </a>
+          </div>
+          <div className="donor-logo">
+            <a href="https://www.redcross.sg/" target="_blank" rel="noreferrer">
+              <img
+                src="assets/images/current-donors/Singapore_Red_Cross.jpg"
+                alt="singapore-rc-logo"
+              />
+            </a>
+          </div>
+          <div className="donor-logo">
+            <a href="https://www.redcross.ca/" target="_blank" rel="noreferrer">
+              <img
+                src="assets/images/current-donors/canada_rc.png"
+                alt="canada-rc-logo"
+              />
+            </a>
+          </div>
+          <div className="donor-logo">
+            <a
+              href="https://www.qrcs.org.qa/en/Pages/default.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="assets/images/current-donors/qatar-red-crescent-logo.png"
+                alt="qatar-rc-logo"
+              />
+            </a>
+          </div>
+          <div className="donor-logo">
+            <a
+              href="https://www.microsoft.com/en-lk"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="assets/images/current-donors/microsoft-logo.png"
+                alt="microsoft-logo"
+              />
+            </a>
+          </div>
+          <div className="donor-logo">
+            <a
+              href="https://www.uber.com/lk/en/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="assets/images/current-donors/uber-logo.png"
+                alt="uber-logo"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </Page>
