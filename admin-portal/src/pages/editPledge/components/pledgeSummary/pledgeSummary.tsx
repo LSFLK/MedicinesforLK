ffb1827c8dbc9aafb18ed4b32a36a236a1b2df52
Manoj Lakshan <48247516+ManojLL@users.noreply.github.com>
2022-08-07 13:24:23 +0530

    Validate pledge status (#350)
    * add validation in pledge status
    
    * formate code
    
    * add data types to the map
    
    * remove function
    
    * minor change

--- admin-portal/src/pages/editPledge/components/pledgeSummary/pledgeSummary.tsx
+++ admin-portal/src/pages/editPledge/components/pledgeSummary/pledgeSummary.tsx
@@ -1,15 +1,36 @@
 import React, { ChangeEvent } from "react";
 import "./pledgeSummary.css";
 import { Pledge } from "../../../../types/Pledge";
+import { AidPackage } from "../../../../types/AidPackage";
 
 interface PledgeSummaryProps {
   pledge: Pledge;
   onStatusChange: (status: Pledge.Status) => void;
+  aidPackageStatus: AidPackage.Status;
 }
+
+const pledgeStatusToAllowedStatuses: Record<
+  Pledge.Status,
+  AidPackage.Status[]
+> = {
+  [Pledge.Status.Pledged]: [
+    AidPackage.Status.Published,
+    AidPackage.Status.AwaitingPayment,
+  ],
+  [Pledge.Status.PaymentInitiated]: [AidPackage.Status.AwaitingPayment],
+  [Pledge.Status.PaymentConfirmed]: [
+    AidPackage.Status.Delivered,
+    AidPackage.Status.Ordered,
+    AidPackage.Status.ReceivedAtMOH,
+    AidPackage.Status.Shipped,
+    AidPackage.Status.AwaitingPayment,
+  ],
+};
 
 export default function PledgeSummary({
   pledge,
   onStatusChange,
+  aidPackageStatus,
 }: PledgeSummaryProps) {
   const handleStatusChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const newStatus = event.currentTarget.value as Pledge.Status;
@@ -34,7 +55,15 @@ export default function PledgeSummary({
       <div>
         <select onChange={handleStatusChange} value={pledge.status}>
           {Object.entries(Pledge.Status).map(([key, status]) => (
-            <option key={key} value={status}>
+            <option
+              key={key}
+              value={status}
+              disabled={
+                !pledgeStatusToAllowedStatuses[status].includes(
+                  aidPackageStatus
+                )
+              }
+            >
               {status}
             </option>
           ))}
