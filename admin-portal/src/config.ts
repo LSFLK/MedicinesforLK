4759273784f0223c547229f90457537142cdfa32
Brion <info@brionmario.com>
2022-08-22 16:54:50 +0530

    Revert back to `Storage.SessionStorage`,

--- admin-portal/src/config.ts
+++ admin-portal/src/config.ts
@@ -7,7 +7,9 @@ const SDKConfig: AuthReactConfig & STSClientConfig = {
   signInRedirectURL: `${process.env.REACT_APP_SIGN_IN_REDIRECT_URL}`,
   signOutRedirectURL: `${process.env.REACT_APP_SIGN_OUT_REDIRECT_URL}`,
   scope: ["openid", "email", "profile"],
-  storage: Storage.WebWorker,
+  // Revert this back to `Storage.WebWorker` once,
+  // https://github.com/asgardeo/asgardeo-auth-react-sdk/issues/120 is fixed.
+  storage: Storage.SessionStorage,
   stsConfig: {
     credentials: {
       client_id: `${process.env.REACT_APP_CLIENT_ID}`,
