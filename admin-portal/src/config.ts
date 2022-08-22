9046d6df5ed92d386e3279e508a5dc728cc4fe34
Brion <info@brionmario.com>
2022-08-22 16:17:32 +0530

    Fix application looping issue in Safari

--- admin-portal/src/config.ts
+++ admin-portal/src/config.ts
@@ -1,9 +1,13 @@
-export default {
+import { AuthReactConfig, Storage } from "@asgardeo/auth-react";
+import { STSClientConfig } from "@asgardeo/token-exchange-plugin";
+
+const SDKConfig: AuthReactConfig & STSClientConfig = {
   clientID: "W_ZaTLGR20OKy0HTYPqSAJvuS5Ia",
   baseUrl: "https://api.asgardeo.io/t/elixir",
   signInRedirectURL: `${process.env.REACT_APP_SIGN_IN_REDIRECT_URL}`,
   signOutRedirectURL: `${process.env.REACT_APP_SIGN_OUT_REDIRECT_URL}`,
   scope: ["openid", "email", "profile"],
+  storage: Storage.WebWorker,
   stsConfig: {
     credentials: {
       client_id: `${process.env.REACT_APP_CLIENT_ID}`,
@@ -14,5 +18,6 @@ export default {
   },
   stsTokenEndpoint: "https://sts.choreo.dev/oauth2/token",
   resourceServerURLs: [`${process.env.REACT_APP_ADMIN_BACKEND_URL}`],
-  disableTrySignInSilently: false,
 };
+
+export default SDKConfig;
