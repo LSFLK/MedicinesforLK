cf68aec1a2eb0b26979ec7e5af83142c37a3993c
Tharindu37 <tharindut520@gmail.com>
2022-12-31 15:06:59 +0530

    Change

--- admin-portal/src/pages/packageDetails/components/packageStatus/packageStatus.tsx
+++ admin-portal/src/pages/packageDetails/components/packageStatus/packageStatus.tsx
@@ -28,7 +28,12 @@ export default function PackageStatus({
                   });
                 }}
               />
-              <label style={{color:status===currentStatus?'black':'gray'}}  htmlFor={key}>{status}</label>
+              <label
+                style={{ color: status === currentStatus ? "black" : "gray" }}
+                htmlFor={key}
+              >
+                {status}
+              </label>
               {index + 1 < Object.entries(AidPackage.Status).length && (
                 <span>&nbsp;&gt;&nbsp;</span>
               )}
