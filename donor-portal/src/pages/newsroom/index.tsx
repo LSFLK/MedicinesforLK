46f060bac293bc19d8b4deb49b58cece3572d103
sanjana0319 <asanjana0823@gmail.com>
2023-03-24 20:20:53 +0530

    Update description

--- donor-portal/src/pages/newsroom/index.tsx
+++ donor-portal/src/pages/newsroom/index.tsx
@@ -45,8 +45,8 @@ const NewsRoom = React.memo(() => {
             >
               <span className="red">8th March 2023</span>
               <p>
-                Sri Lanka Red Cross Society's medical donation matching
-                platform, "ELIXIR", takes pride in the contribution of USD
+                Sri Lanka Red Cross Society&apos;s medical donation matching
+                platform, &quot;ELIXIR&quot;, takes pride in the contribution of USD
                 2467.53 by الهلال الاحمر القطري Qatar Red Crescent, which was
                 donated on March 8, 2023, in support of the ongoing crisis.
               </p>
